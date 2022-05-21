af7eb7f43429be699ff5df1e406025f4142d3459
mitaligemini <Mitali.Varshney@Gemini.com>
2022-05-21 20:49:12 +0530

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { TransactionComponent } from './transaction/transaction.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginPageComponent},
+  {path:'transaction' ,component:TransactionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
