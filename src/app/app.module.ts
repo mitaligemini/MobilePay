feb1306a56f14bfa05ff1f50d1dc7e4b63b6a21b
mitaligemini <Mitali.Varshney@Gemini.com>
2022-05-21 20:52:05 +0530

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransactionComponent } from './transaction/transaction.component';
+import { TransactionsService } from './transactions.service';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TransactionComponent
+    TransactionComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [TransactionsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
